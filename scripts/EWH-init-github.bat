c652abf17b2704a7d33ffcc5468e48b8a6372b6b
ndqphuong <nhudinhquocphuong@gmail.com>
2016-06-30 13:52:02 +0700

    remove echo rác

--- scripts/EWH-init-github.bat
+++ scripts/EWH-init-github.bat
@@ -11,24 +11,22 @@ call :GETTEMPNAME
 git init
 git remote add origin %REPO%
 
-echo ---add build dir vo .gitignore file
+rem add build dir vo .gitignore file
 echo %BUILD_DIR%/>>.gitignore
-echo ---commit and push to master
+rem commit and push to master
 git add .
 git commit -m "init"
 git push --set-upstream origin master
 
-echo ---create build dir
+rem create build dir
 mkdir %BUILD_DIR%
-echo ---clone repo to build dir
+rem clone repo to build dir
 cd %BUILD_DIR%
 git clone %REPO% .
 
-echo ---create and switch to gh-pages branch
+rem create and switch to gh-pages branch
 git checkout -b gh-pages
 
-echo ---remove .gitignore rac' tu` master
-rem del /Q .gitignore
 rem delete all folder not .git in working dir
 for /D %%F in (*) do (
    if NOT %%F == ".git" rd %%F /Q /S
@@ -38,20 +36,20 @@ for %%F in (*) do (
    del %%F /Q /S
 )
 
-echo ---set gh-pages as default remote branch cua repo trong folder build
+rem set gh-pages as default remote branch cua repo trong folder build
 git push --set-upstream origin gh-pages
 
-echo ---delete master branch in build folder
+rem delete master branch in build folder
 git branch -d master
 
-echo ---push changes to remote
+rem push changes to remote
 git add .
 git commit -m "init"
 git push
 
-echo ---back to site repo dir
+rem back to site repo dir
 cd ..
-echo ---install all deps
+rem install all deps
 npm install
 
 
