bd86eb42e9b6a0c0193f8fddefc6cb8a488e2dce
nemesisqp <quocphuong8x@gmail.com>
2017-01-24 15:30:47 +0700

    test remove force (-f) from git sync

--- scripts/EWH-push-github.bat
+++ scripts/EWH-push-github.bat
@@ -4,10 +4,10 @@ set datetime=%date% %time%
 
 git branch --set-upstream-to=origin/master
 rem pull update from remote, use ours changes if conflict happen
-git pull -s recursive -X theirs
+git pull -s recursive -Xtheirs
 rem add all files
 git add .
 rem commit
 git commit -m "%datetime%"
 rem push local commit to remote force use mine
-git push -f origin master
+git push origin HEAD:master
